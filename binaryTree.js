be6d98a812597fd3a857567d298ccf1b2f0e58a8
weiyu.lin <weiyu.in@rakuten.com>
2016-02-29 23:36:02 +0800

    tab to spaces

--- binaryTree.js
+++ binaryTree.js
@@ -1,59 +1,70 @@
 var Node = require("./node");
 
 var BinaryTree = function() {
-	this.root = null;
-	this.insert = insert;
-	this.remove = remove;
-	this.inOrder = inOrder;
+    this.root = null;
+    this.insert = insert;
+    this.remove = remove;
+    this.inOrder = inOrder;
 }
 
 function insert(data) {
-	var that = this;
-	var node = new Node({
-		data: data,
-		left: null,
-		right: null
-	});
+    var that = this;
+    var node = new Node({
+        data: data,
+        left: null,
+        right: null
+    });
 
-	if (that.root === null) {
-		that.root = node;
-		return;
-	} else {
-		var currentNode = that.root;
-		while (true) {
-			if (data > currentNode.data) {
-				if (currentNode.right !== null) {
-					currentNode = currentNode.right;
-				} else {
-					currentNode.right = node;
-					break;
-				}
+    if (that.root === null) {
+        that.root = node;
+        return;
+    } else {
+        var currentNode = that.root;
+        while (true) {
+            if (data > currentNode.data) {
+                if (currentNode.right !== null) {
+                    currentNode = currentNode.right;
+                } else {
+                    currentNode.right = node;
+                    break;
+                }
 
-			} else {
-				if (currentNode.left !== null) {
-					currentNode = currentNode.left;
-				} else {
-					currentNode.left = node;
-					break;
-				}
+            } else {
+                if (currentNode.left !== null) {
+                    currentNode = currentNode.left;
+                } else {
+                    currentNode.left = node;
+                    break;
+                }
 
-			}
-		}
-	}
+            }
+        }
+    }
 }
 
 function inOrder(node) {
-	if (node !== null) {
-		inOrder(node.left);
-		node.show();
-		inOrder(node.right);
-	}
+    if (node !== null) {
+        inOrder(node.left);
+        node.show();
+        inOrder(node.right);
+    }
 }
 
+function _removeNode(node, data) {
+    console.log("remove");
+    if (node !== null) {
+        inOrder(node.left);
+        node.show();
+        inOrder(node.right);
+    }
 
-function remove(data) {
-	console.log("remove");
-	//TODO
+    //TODO
+
+}
+
+function remove(node, data) {
+    console.log("remove");
+    //TODO
 
 }
 
